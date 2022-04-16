cffcf74d4ce7c319b8333497b16bfb731ca48698
cucucucumber <zijian.song1996@gmail.com>
2022-04-15 23:38:12 -0700

    minor changes

--- src/serverM.cpp
+++ src/serverM.cpp
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 	int newfd, maxfd, wallet, rand_pick;
 	int last_serial = 0;
 	char buffer[1024] = {0};
-	bool found, valid, invalid_receiver;
+	bool found, valid, invalid_receiver, invalid_sender;
 	string client_name;
 	string return_msg;
 	string backend_msg;
@@ -347,6 +347,7 @@ int main(int argc, char *argv[])
             		valid = true;
             		found = false;
             		invalid_receiver = true;
+            		invalid_sender = true;
 	            	wallet = 1000;
 	            	cout << "The main server received from " << requests[0] \
 	            	<< " to transfer  " << requests[2] << " coins to " << requests[1] << \
@@ -375,6 +376,7 @@ int main(int argc, char *argv[])
 
 			            if(converted.compare("usr_not_found") != 0){
 			            	found = true;
+			            	invalid_sender = false;
 			            	wallet += stoi(converted);
 			            }
 	            	}
@@ -423,7 +425,6 @@ int main(int argc, char *argv[])
 
 				            if(converted.compare("usr_not_found") != 0){
 				            	found = true;
-				            	invalid_receiver = false;
 				            	// set the last transaction number
 				            	last_serial = max(last_serial, stoi(converted));
 
@@ -473,11 +474,11 @@ int main(int argc, char *argv[])
 	            		return_msg += " ";
 
 	            	} else {
-	            		if(valid && return_msg.compare("invalid_sender") != 0 && invalid_receiver)
+	            		if(valid && !invalid_sender && invalid_receiver)
 	            		{
 	            			return_msg = "invalid_receiver";
 	            			return_msg += " ";
-	            		} else if (valid && return_msg.compare("invalid_sender") == 0 && invalid_receiver) {
+	            		} else if (valid && invalid_sender && invalid_receiver) {
 	            			return_msg = "both_invalid";
 	            			return_msg += " ";
 	            		}
